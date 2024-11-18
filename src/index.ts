517a3b75f2b62799e8a462df041892c1eccb37ec
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2024-11-18 18:35:36 +0800

    feat: setup index file

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import bodyParser from "body-parser";
+import {router} from "./routes";
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 8000;
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/api', router)
+app.use((req, res, next) => {
+    res.send("Route Not Found")
+})
+
+app.listen(port, () => {
+    console.log('Listening on port', port)
+})
+
