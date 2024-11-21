02ff0e8fd400c757f59c7de545a6bf7e54a6e0ff
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2024-11-21 20:42:17 +0800

    feat: add upload file utils

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import bodyParser from "body-parser";
 import {router} from "./routes";
 import {sendResponse} from "./utils/sendResponse";
+import path from "path";
 const app = express()
 dotenv.config()
 const port = process.env.PORT || 5000;
@@ -12,6 +13,8 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', router)
+app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(`${__dirname}/upload`))
 app.use((req, res, next) => {
     return sendResponse(res, false, null, "Routes not Found", 404)
 })
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,20 @@ import {authRoute} from "./auth.route";
 import {authMiddleware} from "../middleware/auth";
 import {ProductCategoryRoute} from "./productCategory.route";
 import {ProductBrandRoute} from "./productBrand.route";
+import {uploadFile} from "../utils/uploadFile";
+import {sendResponse} from "../utils/sendResponse";
 
 export const router = express.Router()
 router.use('/auth', authRoute)
+router.post('/upload', uploadFile.single("image"), (req, res) => {
+    if (!req.file) {
+        return sendResponse(res, false, null, "upload failed", 400)
+    } else {
+        const filePath = `public/upload/${req.file.filename}`;
+        console.log('File path:', filePath);
+        return sendResponse(res, true, null ,"upload file success" , 200)
+    }
+})
 
 router.use('/product-category', authMiddleware, ProductCategoryRoute)
 router.use('/product-brand', authMiddleware, ProductBrandRoute)
